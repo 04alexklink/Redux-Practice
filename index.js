cb1ec862a1ddd21c364fec079daa988ff864fae8
04alexklink <alex_klink123@hotmail.co.uk>
2021-03-28 15:05:16 +0100

    reducer function created, for action type BUY_CAKE, new state object is returned with noOfcakes property equal to previous value - 1

--- index.js
+++ index.js
@@ -7,3 +7,22 @@ buyCake = () => {
     info: 'first redux action'
   }
 }
+
+// reducer
+// (previousState, action) => newState
+const initialState = {
+    numberOfCakes : 10
+}
+
+// define reducer function
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case BUY_CAKE:
+      return {
+        ... state,
+        numberOfCakes: state.numberOfCakes - 1
+      }
+    default: return state;
+  }
+}
